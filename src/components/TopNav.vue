8aee6da5e452b5c5cdf4cd515551100f45801f51
brnkgabriel <brnkgabriel@gmail.com>
2022-12-31 14:24:54 +0100

    css: adjusted sitename on top bar

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -3,9 +3,9 @@
     <NuxtLink href="/" class="">
       <img class="h-[32px]" src="/images/rcn-logo.png" alt="rcn logo"/>
     </NuxtLink>
-    <div aria-label="site name" :class="center + ' ' + flex_center">
-      <div class="uppercase font-bold font-title" :class="mainline">children</div>
-      <div class="uppercase font-bold font-title" :class="subline">department</div>
+    <div aria-label="site name" :class="sitename">
+      <div>children</div>
+      <div>department</div>
     </div>
     <div aria-label="notification" class="rounded-full border-2 border-bglight-700 p-[2px] h-[40px] w-[40px] cursor-pointer" @click="toggleDropdown">
       <img class="rounded-full" :src="imgSrc(props.avatar as string)" alt="avatar"/>
@@ -15,9 +15,10 @@
 </template>
 <script setup lang="ts">
 
-const { subline, mainline, center, flex_center } = useUi()
+const { mainline, center, flex_center } = useUi()
 
 const show = ref(false)
+const sitename = ref(`${center} ${flex_center} ${mainline} uppercase font-bold font-title`)
 
 const toggleDropdown = () => {
   console.log("toggling dropdown") 
