1b1e7c5142039ca6abaf9772b500f425c08287fa
brnkgabriel <brnkgabriel@gmail.com>
2023-01-01 21:48:02 +0100

    fix: profile page css

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -4,8 +4,8 @@
       class="w-full h-[100px] rounded"
       :src="props.student.imageUrl"
       :alt="props.student.firstName + ' ' + props.student.lastName"/>
-    <div class="p-1/2 flex items-center justify-center font-title w-full overflow-hidden text-ellipsis whitespace-nowrap absolute text-white bottom-0 left-1/2 -translate-x-1/2 bg-rcngray-100">
-      <span>{{ props.student.firstName }}</span>
+    <div class="p-1/2 flex items-center justify-center font-title w-full absolute text-white bottom-0 left-1/2 -translate-x-1/2 bg-rcngray-100">
+      <span class="w-3/4 text-center overflow-hidden text-ellipsis whitespace-nowrap">{{ props.student.firstName }}</span>
     </div>
   </div>
 </template>
