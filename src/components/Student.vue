0c749c3160390a0b42bd236f704700431a003568
brnkgabriel <brnkgabriel@gmail.com>
2023-01-01 21:36:12 +0100

    fix: css for profile page

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="bg-white rounded shadow-custom inline-block align-top mx-1 cursor-pointer w-[80px]">
+  <div class="bg-white rounded shadow-custom inline-block align-top mx-1 cursor-pointer w-[100px] relative overflow-hidden">
     <img
-      class="w-full h-[80px] rounded"
+      class="w-full h-[100px] rounded"
       :src="props.student.imageUrl"
       :alt="props.student.firstName + ' ' + props.student.lastName"/>
-    <div class="text-center px-2 py-1 font-title w-full overflow-hidden text-ellipsis whitespace-nowrap">{{ props.student.firstName }}</div>
+    <div class="p-1/2 flex items-center justify-center font-title w-full overflow-hidden text-ellipsis whitespace-nowrap absolute text-white bottom-0 left-1/2 -translate-x-1/2 bg-rcngray-100">
+      <span>{{ props.student.firstName }}</span>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
